725c2e5c068106d72577ae693558379724e3e7b6
RaimundoVU <raimundovu97@gmail.com>
2024-06-04 22:50:46 -0400

    add config for null-ls (now is none-ls)

--- .config/newnvim/lua/plugins/lsp-config.lua
+++ .config/newnvim/lua/plugins/lsp-config.lua
@@ -26,7 +26,8 @@ return {
       lspconfig.tsserver.setup({})
 
       vim.keymap.set('n', 'K', vim.lsp.buf.hover, {})
-      vim.keymap.set('n', 'gd', vim.lsp.buf.definition, {})
+      vim.keymap.set('n', '<leader>gd', vim.lsp.buf.definition, {})
+      vim.keymap.set('n', '<leader>gr', vim.lsp.buf.references, {})
       vim.keymap.set('n', '<leader>ca', vim.lsp.buf.code_action, {})
 
 
