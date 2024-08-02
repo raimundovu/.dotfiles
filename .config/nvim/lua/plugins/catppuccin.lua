1ab0f13b42e3e38ff86d6d54b4491815b55d4ebf
Raimundo Vásquez <rvasquezu@falabella.cl>
2024-08-02 12:19:36 -0400

    feat: changing visual settings
    Added catppuccin frape flavour for alacritty and nvim

--- .config/nvim/lua/plugins/catppuccin.lua
+++ .config/nvim/lua/plugins/catppuccin.lua
@@ -4,7 +4,8 @@ return {
   priority = 1000,
   config = function()
     require("catppuccin").setup({
-      transparent_backgroud = true
+      flavour = "frappe",
+      transparent_background = true,
     })
     vim.cmd.colorscheme "catppuccin"
   end
