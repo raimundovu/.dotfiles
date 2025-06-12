63c44f51fe8df185fc3c761894b3ee173b32eb94
raimundovu <raimundovu97@gmail.com>
2025-06-11 23:10:50 -0400

    visual changes, add peek, no stow required

--- .config/nvim/lua/plugins/catppuccin.lua
+++ .config/nvim/lua/plugins/catppuccin.lua
@@ -4,7 +4,7 @@ return {
   priority = 1000,
   config = function()
     require("catppuccin").setup({
-      flavour = "frappe",
+      flavour = "macchiato",
       transparent_background = true,
     })
     vim.cmd.colorscheme "catppuccin"
